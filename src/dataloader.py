00fd1759ece6f8e4d96793f23bddea0c34c4adde
androiderNN <kento.i.1103@gmail.com>
2024-09-07 00:33:41 +0900

    dataloaderの実装

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from torchvision.io import ImageReadMode, read_image
 import matplotlib.pyplot as plt
 
@@ -38,18 +39,33 @@ class labelconverter():
         return label_list
 
 class customDataset(Dataset):
-    def __init__(self):
-        table = pd.read_table(os.path.join(config.data_path, 'train_master.tsv'))
-        label = table['expression'].tolist()
-        lc = labelconverter(label)
-        self.label_array = lc.encode(label)
-
-        image_names = os.listdir(config.train_path)
-        image_names.sort()
-        self.image_array = [read_image(os.path.join(config.train_path, i), mode=ImageReadMode.GRAY)/255 for i in image_names]
+    def __init__(self, image, label, index):
+        self.image_array = image[index]
+        self.label_array = label[index]
 
     def __len__(self):
-        return len(self.image_array)
+        return len(self.label_array)
     
     def __getitem__(self, idx):
         return {'image':self.image_array[idx], 'label': self.label_array[idx]}
+
+def get_dataloaders(train_size=0.8, valid_size=0.1, test_size=0.1, batch_size=10):
+    # 画像の読み込みと格納
+    image_names = os.listdir(config.train_path)
+    image_names.sort()
+    image_array = [read_image(os.path.join(config.train_path, i), mode=ImageReadMode.GRAY)/255 for i in image_names]
+
+    # ラベルの読み込みとone-hot encoding
+    table = pd.read_table(os.path.join(config.data_path, 'train_master.tsv'))
+    label_array = labelconverter(table['expression'].tolist())
+
+    # インデックス分割
+    index = [i for i in range(len(image_names))]
+    train_index, index = train_test_split(index, train_size=train_size, shuffle=True)
+    valid_index, test_index = train_test_split(index, train_size=valid_size, valid_size=test_size, shuffle=True)
+
+    # dataloader作成
+    index_array = [train_index, valid_index, test_index]
+    dataloaders = [DataLoader(customDataset(image_array, label_array, i), batch_size=batch_size) for i in index_array]
+
+    return dataloaders
