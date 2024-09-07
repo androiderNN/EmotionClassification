c466aac24d87dc55d1fe11a372ae4777111c16d8
androiderNN <kento.i.1103@gmail.com>
2024-09-07 22:10:02 +0900

    CNNの簡易実装

--- src/net.py
+++ src/net.py
@@ -24,6 +24,33 @@ class flatNet(nn.Module):
         y = self.linear_stack(x)
         return y
 
+class simpleConvNet(nn.Module):
+    def __init__(self):
+        super(simpleConvNet, self).__init__()
+        
+        channels = {
+            'channel1': 16,
+            'channel2': 32,
+            'channel3': 64
+        }
+
+        self.conv_stack = nn.Sequential(
+            nn.Conv2d(1, channels['channel1'], kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(channels['channel1']),
+            nn.ReLU(),
+            nn.Conv2d(channels['channel1'], channels['channel2'], kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(channels['channel2']),
+            nn.ReLU(),
+            nn.Conv2d(channels['channel2'], channels['channel3'], kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(channels['channel3']),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(int((channels['channel3']*128*120)/(8**2)), 4)
+        )
+    
+    def forward(self, x):
+        return self.conv_stack(x)
+
 def train_loop(dataloader, model, loss_fn, optimizer):
     model.train()
     size = len(dataloader.dataset)
@@ -57,16 +84,16 @@ if __name__=='__main__':
     train_params = {
         'lr': 1e-2,
         'batch_size': 100,
-        'epochs':5
+        'epochs':10
     }
 
-    model = flatNet()
+    model = simpleConvNet()
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=train_params['lr'])
     
     train_dataloader, valid_dataloader, _ = dataloader.get_dataloaders(batch_size=train_params['batch_size'])
 
     for e in range(train_params['epochs']):
-        print(f'\nepoch {e}')
+        print(f'epoch {e}')
         train_loop(train_dataloader, model, loss_fn, optimizer)
         test_loop(valid_dataloader, model, loss_fn)
