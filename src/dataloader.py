e12f8c181910eea5c7f3d1d61b8d49aa1c2cf546
androiderNN <kento.i.1103@gmail.com>
2024-09-07 20:06:10 +0900

    train_loopが回らない問題の修正 datasetの__getitem__()について、Pytorch公式colabではdictを返す実装であったが、これだとkeyがimageとして 使用されており（たとえばimageがトレーニングデータとして送られており）エラーが出ていた。 (image, label)を返すとひとまずエラーはでなくなった. そのほかラベルエンコーディングの方式の変更など

--- src/dataloader.py
+++ src/dataloader.py
@@ -25,31 +25,45 @@ class labelconverter():
     def encode(self, label_list):
         '''
         ラベル名のリストを渡すとencodingした(要素数,ラベル数)のndarrayを返す'''
-        label_array = np.zeros((len(label_list), self.num_label))
-        for i, label in enumerate(label_list):
-            label_array[i, self.encode_dict[label]] = 1
+        # label_array = np.zeros((len(label_list), self.num_label))
+        # for i, label in enumerate(label_list):
+        #     label_array[i, self.encode_dict[label]] = 1
+
+        ##
+        label_array = label_list
+        for value, i in self.encode_dict.items():
+            label_array = [i if l==value else l for l in label_array]
 
         return label_array
 
     def decode(self, label_array):
         '''
         encodeされたndarrayを渡すとラベル名のリストに変換する'''
-        label_array = np.argmax(label_array, axis=1)
-        label_list = [self.decode_dict(label_list[l]) for l in label_array]
+        # label_array = np.argmax(label_array, axis=1)
+        # label_list = [self.decode_dict(label_list[l]) for l in label_array]
+        label_list = list(label_array)
+        for i, value in self.encode_dict.items():
+            label_list = [value if l==i else l for l in label_list]
         return label_list
 
 class customDataset(Dataset):
     def __init__(self, image, label, index):
-        self.image_array = image[index]
-        self.label_array = label[index]
+        self.image_array = [image for idx, image in enumerate(image) if idx in index]
+        self.label_array = [label for idx, label in enumerate(label) if idx in index]
 
     def __len__(self):
         return len(self.label_array)
     
     def __getitem__(self, idx):
-        return {'image':self.image_array[idx], 'label': self.label_array[idx]}
+        image = self.image_array[idx]
+        label = self.label_array[idx]
+        # return {'image':image, 'label': label}
+        return (image, label)
 
 def get_dataloaders(train_size=0.8, valid_size=0.1, test_size=0.1, batch_size=10):
+    if train_size+valid_size+test_size>1:
+        raise ValueError
+
     # 画像の読み込みと格納
     image_names = os.listdir(config.train_path)
     image_names.sort()
@@ -57,15 +71,18 @@ def get_dataloaders(train_size=0.8, valid_size=0.1, test_size=0.1, batch_size=10
 
     # ラベルの読み込みとone-hot encoding
     table = pd.read_table(os.path.join(config.data_path, 'train_master.tsv'))
-    label_array = labelconverter(table['expression'].tolist())
+    label = table['expression'].tolist()
+    lc = labelconverter(label)
+    label_array = lc.encode(label)
 
     # インデックス分割
     index = [i for i in range(len(image_names))]
     train_index, index = train_test_split(index, train_size=train_size, shuffle=True)
-    valid_index, test_index = train_test_split(index, train_size=valid_size, valid_size=test_size, shuffle=True)
+    valid_index, test_index = train_test_split(index, train_size=valid_size, test_size=test_size, shuffle=True)
 
     # dataloader作成
     index_array = [train_index, valid_index, test_index]
-    dataloaders = [DataLoader(customDataset(image_array, label_array, i), batch_size=batch_size) for i in index_array]
+    datasets = [customDataset(image_array, label_array, i) for i in index_array]
+    dataloaders = [DataLoader(ds, batch_size) for ds in datasets]
 
     return dataloaders
